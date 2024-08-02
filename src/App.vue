3606ba0836da65af3454172180bb7cdf688df592
Owen <jaewoo.song@dtonic.io>
2024-08-02 10:28:18 +0900

    Init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppHeader />
+  <div id="main-wrapper">
+    <RouterView />
+  </div>
+</template>
+<script>
+import AppHeader from '@/views/app/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+<style scoped>
+#main-wrapper {
+  padding-top: 80px;
+  padding-bottom: 0px;
+  min-height: calc(100% - 80px);
+  height: 100%;
+  overflow-y: auto;
+}
+</style>
